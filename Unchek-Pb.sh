b7bf70bcdf5ae0ad585922d2c0a2b5a76aa4790e
MedanXploit12 <75316440+MRKALIT09@users.noreply.github.com>
2021-02-17 22:29:19 +0700

    Add files via upload

---
+++ Unchek-Pb.sh
@@ -0,0 +1,159 @@
+#!bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+
+echo $cy "   INI ADALAH SCRIPT CHEKER ALL GAME  "
+echo $cy "   ADA BANYAK MENU PILIHAN SCRIPT NYA BOS KU   "
+echo $me "   JIKA CHEKER ALL GAME  NYA SALAH AUTHOR TIDAK TANGGUNG JAWAB "
+echo $me "   JIKA SCRIPT RUSAK/ERROR HUB SAYA DI MENU TOOLS NYA "
+echo $ku " ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁"
+echo
+echo $bi  "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁"
+echo
+echo $pur "   CREATOR BY     : $ku ./MANDO-XPLOITER "
+echo $pur "   WhatsApp ME    : $ku  0853-6152-4681  "
+echo $pur "   MY TEAM CYBER  : $ku  MEDAN-XPLOITER  "
+echo $pur "   GITHUB ME      : $ku  github.com/MRKALIT09 "
+echo $bi  "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁"
+echo
+echo $i "  ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ "
+echo
+echo $i  "         HARAP MASUKAN MENU TOOLS NYA    "
+echo
+echo $cy "        [1] :$ku CHEKER POINT BLANK      "
+echo $cy "        [2] :$ku CHEKER FREEFIRE         "
+echo $cy "        [3] :$ku CHEKER MOBILE LEGENDS   "
+echo $cy "        [4] :$ku HUBUNGIN ADMIN VIA FB   "
+echo $cy "        [5] :$ku HUBUNGIN ADMIN VIA WA   "
+echo $i  " ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁"
+read -p "          MASUKAN MENU TOOLS : " pill
+if [ $pill = "1" ]
+then
+echo $me "         [✓]$i START CRACK ..."
+sleep 3
+echo
+echo $i  " SILAHKAN DI CEK "
+echo $cy " USERNAME :$i rahsansinta"
+echo $cy " PASSWORD :$i sansinta878"
+echo $pur" -------------------------"
+sleep 5
+echo
+echo $i  " SILAHKAN DI CEK "
+echo $cy " USERNAME :$i aizkusman732"
+echo $cy " PASSWORD :$i kuskusman732"
+echo $pur" -------------------------"
+sleep 5
+echo
+echo $i  " SILAHKAN DI CEK "
+echo $cy " USERNAME :$i riniwijay989"
+echo $cy " PASSWORD :$i wijaya098"
+echo $pur" -------------------------"
+sleep 5
+echo $i  " SILAHKAN DI CEK"
+echo $cy " USERNAME :$i ayatfara62"
+echo $cy " PASSWORD :$i myclanfar4"
+echo $pur" -------------------------"
+sleep 5
+echo $i  " SILAHKAN DI CEK "
+echo $cy " USERNAME :$i martinstuard"
+echo $cy " PASSWORD :$i stuardfam7"
+echo $pur" -------------------------"
+sleep 5
+echo $i  " SILAHKAN DI CEK "
+echo $cy " USERNAME :$i syahrulmutafifin"
+echo $cy " PASSWORD :$i 1234fatimah"
+echo $pur" -------------------------"
+sleep 5
+echo $i  " SILAHKAN DI CEK "
+echo $cy " USERNAME :$i rafighifari17"
+echo $cy " PASSWORD :$i rafidarsal18"
+echo $pur" -------------------------"
+sleep 5
+echo $i  " SILAHKAN DI CEK"
+echo $cy " USERNAME :$i manganang990"
+echo $cy " PASSWORD :$i mangana56"
+echo $pur" -------------------------"
+sleep 5
+echo $i  " SILAHKAN DI CEK"
+echo $cy " USERNAME :$i Chand22pfft"
+echo $cy " PASSWORD :$i topangile88"
+echo $pur" ------------------------"
+sleep 5
+echo $i  " SILAHKAN DI CEK"
+echo $cy " USERNAME :$i stevankape"
+echo $cy " PASSWORD :$i tukangojek1"
+echo $pur" ------------------------"
+sleep 10
+echo $me " [!]$ku PROSES CHEKER PB SUDAH SELESAI "
+sleep 4
+echo $me " [!]$ku ANDA AKAN LOG OUT...."
+sleep 4
+echo $me " [✓]$i  LOG OUT SUCCES BOS KU..."
+fi
+#batas gan
+if [ $pill = "2" ]
+then
+echo $me " SILAHKAN DI CEK"
+echo $cy " EMAIL AKUN :$i alyunus06@gmail.com"
+echo $cy " PASSWORD   :$i 0607199022"
+echo $pur"-------------------------"
+echo
+sleep 5
+echo $i  " SILAHKAN DI CEK"
+echo $cy " EMAIL AKUN :$i mukulverma49@gmail.com"
+echo $cy " PASSWIRD   :$i mucool14 "
+echo $pur" ------------------------"
+sleep 5
+echo
+echo $i  " SILAHKAN DI CEK"
+echo $cy " EMAIL AKUN :$i amitsharma.07821@gmail.com"
+echo $cy " PASSWORD   :$i Anjeeta_sharma"
+echo $pur" -------------------------"
+sleep 5
+echo
+echo $i  " SILAHKAN DI CEK "
+echo $cy " EMAIL AKUN :$i syahrulmutafifin@gmail.com"
+echo $cy " PASSWORD   :$i 1234fatimah"
+echo $pur" -------------------------"
+sleep 5
+echo
+echo $i  " SILAHKAN DI CEK "
+echo $cy " USERNAME   :$i jepryarjuna@gmail.com"
+echo $cy " PASSWORD   :$i 4kuc1nt4d3w1"
+echo
+sleep 15
+echo $ku " [•]$me SEPERTI NYA KONEKSI MU BURUK"
+sleep 20
+echo $ku " [!]$me LOG OUT......"
+sleep 15
+echo $ku " [✓]$i LOG OUT SUUCCES"
+fi
+#batas gan
+if  [ $pill = "3" ]
+then
+echo $me " [!] $bi MOHON MAFF MENU YG INI SEDANG DALAM PERBAIKAN JADI PILIH MENU LAIN YA:)"
+sleep 15
+fi
+#batas gan
+
+if [ $pill = "4" ]
+then
+echo $me " [!]$i KAMU AKAN OTOMATIS DI ARAHKAN KE FACEBOOK SAYA "
+sleep 10
+xdg-open https://www.facebook.com/profile.php?id=100027700041268
+fi
+if [ $pill = "5" ]
+then
+echo $me " [!]$i KAMU AKAN OTOMATIS DI ARAHKAN KE WhatsApp Saya "
+sleep 5
+xdg-open https://wa.me/6285361524681
+fi
+exit
+
